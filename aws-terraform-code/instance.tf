f2333839023be8eea33ad7ba65b4d6e050b9f76a
Harshet jain <hjain8620.hj@gmail.com>
2022-01-01 10:40:45 +0530

    instance code

--- aws-terraform-code/instance.tf
+++ aws-terraform-code/instance.tf
@@ -10,19 +10,17 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-  profile = "new"
+  # Change your profile 
+  profile = "openstack"
 }
-
-
 
 resource "aws_instance" "web" {
   ami           = "ami-0f2e255ec956ade7f"
-  instance_type = "t2.xlarge" 
-  key_name = "new-account"
+  instance_type = "t2.xlarge"
+  #Change your key 
+  key_name = "account-key"
   root_block_device {
-
-    volume_size = 100
-  
+    volume_size = 100 
   }
 
   tags = {
