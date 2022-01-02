07d68bbb717de62bdbbcc8af6beedad78be0a9b4
Harshet jain <hjain8620.hj@gmail.com>
2022-01-02 21:32:19 +0530

    boot script

--- openstack-terraform-code/scripts/boot.sh
+++ openstack-terraform-code/scripts/boot.sh
@@ -2,7 +2,13 @@
 
 #!/bin/bash
 
-sudo su -
-yum install httpd -y
-systemctl enable httpd --now
 
+sudo yum install httpd -y
+sudo systemctl enable httpd --now
+sudo cat << EOF > /var/wwww/html/index.html
+<body bgcolor = "#E0FFFF" >
+<h1>WELCOME TO THE AUTOMATION WORLD</h1>
+<h3>THIS IS TESTING PAGE!!!</h3>
+</body>
+<img src='url' width=200 height=200 />
+EOF
